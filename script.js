e60dab5eba4a563c2da8618257d27f1027766db4
deavetheferret <francois.devantay@studentfr.ch>
2022-03-31 14:58:51 +0200

    try to make the italic toggler ready

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("italic-toggler").onclick = function() {ItalicToggle()};
+
+function ItalicToggle() {
+  document.getElementById("italic-toggle").style.display = "none";
+}
+
+document.getElementById("italic-toggler-1").onclick = function() {ItalicToggle()};
+
+function ItalicToggle() {
+  document.getElementById("italic-toggle-1").style.display = "none";
+}
