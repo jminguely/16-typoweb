5f5778fd94fa869fe9583889fb9bbafbb3c04383
deavetheferret <francois.devantay@studentfr.ch>
2022-04-05 08:39:38 +0200

    fix and done the italic toggle for exhibiton sect.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function ItalicToggleWidth() {
-  document.getElementById("italic-toggle-width").classList.add('italic-active');
+function ToggleItalic() {
+  document.getElementById("toggle-italic").classList.add('italic-active');
 }
 
-function ItalicToggleWeight() {
-  document.getElementById("italic-toggle-weight").classList.add('italic-active');
+function ItalicToggle() {
+  document.getElementById("italic-toggle").classList.add('italic-active');
+}
+
+function ItalicToggleNone() {
+  document.getElementById("italic-toggle").classList.remove('italic-active')
+}
+
+function ToggleItalicNone() {
+  document.getElementById("toggle-italic").classList.remove('italic-active')
 }
