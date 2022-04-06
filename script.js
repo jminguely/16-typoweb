d2ee7abfc9794561808c8dab2d7656043e6693d0
deavetheferret <francois.devantay@studentfr.ch>
2022-04-06 15:14:33 +0200

    start trying to make range work

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ function ItalicToggle() {
   document.getElementById("italic-toggle").classList.toggle('italic-active');
 }
 
-function WidthModifier() {
-  document.getElementById("editable-content").style.fontVariationSettings('"wdth" 80')
+function WidthModifier(e) {
+  console.log('ici', e);
+  // document.getElementById("editable-content").style.fontVariationSettings('"wdth" 10')
+}
+
+function WeightModifier(e) {
+  console.log('ici', e);
+  // document.getElementById("editable-content").style.fontVariationSettings('"wdth" 10')
+}
+
+function ItalicModifier(e) {
+  console.log('ici', e);
+  // document.getElementById("editable-content").style.fontVariationSettings('"wdth" 10')
 }
 
 function MenuSliderOpen() {
