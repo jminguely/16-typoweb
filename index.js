7fa6f19625324719526fe2f32527ec99641e5878
EnoDucotterd <eno.ducotterd@studentfr.ch>
2022-04-12 11:48:25 +0200

    création de variable pour déroulement de contenu en overflow

---
+++ index.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".history-more")
+var documentation = document.querySelector(".history")
+var open = function(){
+  documentation.classList.toggle("is-open")
+}
+button.addEventListener("click",open)
