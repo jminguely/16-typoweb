07df82b6b8e247b955ffae9298548d65101e4251
EnoDucotterd <eno.ducotterd@studentfr.ch>
2022-04-12 16:03:59 +0200

    ajout de variable javascript aux glyphes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var button = document.querySelector(".history-more")
-var documentation = document.querySelector(".history")
+var buttonhistory = document.querySelector(".history-more")
+var documentation1 = document.querySelector(".history")
 var open = function(){
-  documentation.classList.toggle("is-open")
+  documentation1.classList.toggle("is-open")
 }
-button.addEventListener("click",open)
+buttonhistory.addEventListener("click",open)
 
+var buttonglyphes = document.querySelector(".a-glyphes-view")
+var documentation2 = document.querySelector(".all-glyphes")
+var open = function(){
+  documentation2.classList.toggle("is-open")
+}
+buttonglyphes.addEventListener("click",open)
