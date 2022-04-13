de9533e6362b4aaef26ebaa4fde975a4bd4645da
deavetheferret <francois.devantay@studentfr.ch>
2022-04-13 08:59:10 +0200

    try to make the JavaScript work

--- script.js
+++ script.js
@@ -11,38 +11,6 @@ function WidthModifier(e) {
   if (e = 25) {
     document.getElementById("editable-content").style.fontVariationSettings('"wdth" 25')
   }
-
-  if (e > 26) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 26')
-  }
-
-  if (e > 27) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 27')
-  }
-
-  if (e > 28) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 28')
-  }
-
-  if (e > 29) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 29')
-  }
-
-  if (e > 30) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 30')
-  }
-
-  if (e > 31) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 31')
-  }
-
-  if (e > 32) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 32')
-  }
-
-  if (e > 200) {
-    document.getElementById("editable-content").style.fontVariationSettings('"wdth" 200')
-  }
 }
 
 function WeightModifier(e) {
